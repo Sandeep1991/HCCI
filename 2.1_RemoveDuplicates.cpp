44f7000594b54a5711c5de67bc3cb3400d0f6db5
Sandeep1991 <sandeepvadiraj@yahoo.co.in>
2016-05-22 03:22:57 -0700

    Update 2.1_RemoveDuplicates.cpp

--- 2.1_RemoveDuplicates.cpp
+++ 2.1_RemoveDuplicates.cpp
@@ -64,3 +64,67 @@ int main()
 	printList(head);
 	return 0;
 }
+
+
+//Another approach is using sets 
+
+//2.1 Linked list to remove the duplicate items
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+struct node{
+	int data;
+	struct node *next;
+};
+void push(struct node** head, int data)
+{
+	node *temp = new node;
+	temp->data = data;
+	temp->next = (*head);
+	(*head) = temp;
+}
+
+void removedup(node* head)
+{
+	node* temp = head;
+	set<int> myset;
+	set<int>::iterator it;
+	while(temp!=NULL)
+	{
+		if(myset.find(temp->data) == myset.end())
+		{
+			myset.insert(temp->data);
+			temp = temp ->next;
+		}	
+		else
+		{
+			temp = temp->next;
+		}
+		//root=NULL;
+	}	
+	for(set<int>::iterator it=myset.begin();it != myset.end();++it)
+	{
+		cout<<*it<<" ";
+	}
+	cout<<endl;
+	
+}
+
+
+int main()
+{
+	struct node* head = NULL;
+	push(&head,7);
+	push(&head,7);
+	push(&head,3);
+	push(&head,3);
+	push(&head,2);
+	push(&head,2);
+	
+	//printList(head);
+	removedup(head);	
+	//printList(head);
+	return 0;
+}
+
