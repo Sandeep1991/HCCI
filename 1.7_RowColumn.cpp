b8d70543b2a67efa26f727c27d7c815832bebd88
Sandeep1991 <sandeepvadiraj@yahoo.co.in>
2016-05-21 18:54:57 -0700

    Create 1.7_RowColumn.cpp

---
+++ 1.7_RowColumn.cpp
@@ -0,0 +1,54 @@
+//1.7 MxN matrix transform entire row and column to zero if an element is zero in a particular row and column
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	vector< vector<int> > v(n,vector<int>(n));
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			int l;
+			cin>>l;
+			v[i][j] = l;
+		}
+	}
+	vector<int>c(n);
+	vector<int>r(n);
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(v[i][j]==0)
+			{
+				c[j] = 1;
+				r[i] = 1;
+				}	
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(c[j]==1 || r[i] == 1)
+			{
+				v[i][j] = 0;
+				}	
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cout<<v[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
