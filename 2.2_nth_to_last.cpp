67d83fb869b4b39a934eab7591b373b873366bd2
Sandeep1991 <sandeepvadiraj@yahoo.co.in>
2016-05-23 21:55:50 -0700

    Update 2.2_nth_to_last.cpp

--- 2.2_nth_to_last.cpp
+++ 2.2_nth_to_last.cpp
@@ -47,3 +47,62 @@ int main()
 	//printList(head);
 	return 0;
 }
+//2. Print n-1 th to last node using two pointers
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+struct node{
+	int data;
+	struct node *next;
+};
+void push(struct node** head, int data)
+{
+	node *temp = new node;
+	temp->data = data;
+	temp->next = (*head);
+	(*head) = temp;
+}
+void lastbutone(struct node* head,int n)
+{
+	node *p1 = head;
+	node *p2 = head;
+	for(int j=0;j<n-1;++j)
+	{
+		if(p2==NULL)
+		{
+			cout<<"NULL";
+		}
+		p2 = p2->next;
+	}
+	while(p2->next!=NULL)
+	{
+		p1 = p1->next;
+		p2 = p2->next;
+	}
+	cout<<p1->data;
+}
+ 
+void printList(struct node *node)
+{
+	while(node != NULL)
+	{
+		cout<<" "<<node->data;
+		node = node->next;
+	}
+	cout<<endl;
+}
+int main()
+{
+	struct node* head = NULL;
+	push(&head,7);
+	push(&head,6);
+	push(&head,3);
+	push(&head,2);
+	push(&head,2);
+	
+	printList(head);
+	lastbutone(head,3);	
+	//printList(head);
+	return 0;
+}
