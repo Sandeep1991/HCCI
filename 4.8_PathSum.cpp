0ebc83da07dfbb816d5267aa1c02f5478291de1c
Sandeep1991 <sandeepvadiraj@yahoo.co.in>
2016-05-30 23:07:10 -0700

    Create 4.8_PathSum.cpp

---
+++ 4.8_PathSum.cpp
@@ -0,0 +1,83 @@
+//4.8 Root to leaf path sum equal to a given number
+//The implemented version only tells the presence and not gives all possible satisfying
+//values. The complexity is O(n).
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node{
+	int data;
+	struct node* left, *right;
+};
+
+bool hasPathSum(struct node* node, int sum)
+{
+  /* return true if we run out of tree and sum==0 */
+  if (node == NULL)
+  {
+     return (sum == 0);
+  }
+  
+  else
+  {
+    bool ans = 0;  
+  
+    /* otherwise check both subtrees */
+    int subSum = sum - node->data;
+  
+    /* If we reach a leaf node and sum becomes 0 then return true*/
+    if ( subSum == 0 && node->left == NULL && node->right == NULL )
+      return 1;
+  
+    if(node->left)
+      ans = ans || hasPathSum(node->left, subSum);
+    if(node->right)
+      ans = ans || hasPathSum(node->right, subSum);
+  
+    return ans;
+  }
+}
+ 
+/* UTILITY FUNCTIONS */
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+struct node* newnode(int data)
+{
+  struct node* node = (struct node*)
+                       malloc(sizeof(struct node));
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+ 
+  return(node);
+}
+ 
+/* Driver program to test above functions*/
+int main()
+{
+ 
+  int sum = 21;
+ 
+  /* Constructed binary tree is
+            10
+          /   \
+        8      2
+      /  \    /
+    3     5  2
+  */
+  struct node *root = newnode(10);
+  root->left        = newnode(8);
+  root->right       = newnode(2);
+  root->left->left  = newnode(3);
+  root->left->right = newnode(5);
+  root->right->left = newnode(2);
+ 
+  if(hasPathSum(root, sum))
+    printf("There is a root-to-leaf path with sum %d", sum);
+  else
+    printf("There is no root-to-leaf path with sum %d", sum);
+
+  return 0;
+}
